d2e6e75b5073e145d44ff43ca47ab3402df131c0
Gabriel Genê <gabrielgene97@gmail.com>
2017-10-25 22:57:49 -0300

    Improve start server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const users = require('./schema');
 const app = express();
 
-app.use('/user', graphqlHttp({schema:users, pretty: true}))
-app.listen(3000, function() {
+app.use('/user', graphqlHttp({ schema: users, pretty: true }))
+app.listen(3000, function () {
   console.log('Server on');
 });
