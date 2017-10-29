681b5f02af76c8d0b681592ac11373624b97faad
Gabriel Genê <gabrielgene97@gmail.com>
2017-10-29 14:56:28 -0300

    Implement graphql  route and graphiql

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const graphql = require('graphql');
-const graphqlHttp = require('express-graphql');
-const express = require('express');
-const users = require('./schema');
-const app = express();
+const graphql = require('graphql')
+const graphqlHTTP = require('express-graphql')
+const express = require('express')
+const users = require('./schema')
+const app = express()
+const port = 3030;
 
-app.use('/user', graphqlHttp({ schema: users, pretty: true }))
-app.listen(3000, function () {
-  console.log('Server on');
+app.use('/graphql', graphqlHTTP({
+  schema:users,
+  pretty: true,
+  graphiql: true
+}));
+
+app.listen(port, function () {
+  console.log('Listen on port :'+ port);
 });
